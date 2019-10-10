6684164918c6a53000853fa7e29b61b182e49f5c
luis <mrluismartinezzz@gmail.com>
2019-10-10 10:50:38 -0700

    [refactor] from thenables to async :')

--- data/seeds/01_initial_seeds.js
+++ data/seeds/01_initial_seeds.js
@@ -1,29 +1,12 @@
 const f = require('../seedHelpers')
-exports.seed = function(knex) {
-  // Deletes ALL existing entries
-  return knex('sports')
-    .del()
-    .then(function() {
-      // Inserts seed entries
-      return knex('sports')
-        .insert(f.createSportsSeeds)
-        .then(() => {
-          return knex('leagues').insert(f.createLeaguesSeeds)
-        })
-        .then(() => {
-          return knex('allegiances').insert(f.createAllegiancesSeeds)
-        })
-        .then(() => {
-          return knex('users').insert(f.createUsersSeeds)
-        })
-        .then(() => {
-          return knex('groups').insert(f.createGroupsSeeds)
-        })
-        .then(() => {
-          return knex('group_members').insert(f.createGroupMembersSeeds)
-        })
-        .then(() => {
-          return knex('posts').insert(f.createPostsSeeds)
-        })
-    })
+exports.seed = async function(knex) {
+  return Promise.all([
+    await knex('sports').insert(f.createSportsSeeds),
+    await knex('leagues').insert(f.createLeaguesSeeds),
+    await knex('allegiances').insert(f.createAllegiancesSeeds),
+    await knex('users').insert(f.createUsersSeeds),
+    await knex('groups').insert(f.createGroupsSeeds),
+    await knex('group_members').insert(f.createGroupMembersSeeds),
+    await knex('posts').insert(f.createPostsSeeds),
+  ])
 }
